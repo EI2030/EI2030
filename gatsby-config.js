8b8cbbb3476320c4da2af1b15d85b41e08c67155
Matt Schick <matt@schickm.com>
2021-04-30 06:43:24 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
